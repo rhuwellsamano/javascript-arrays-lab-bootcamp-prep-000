24ab0611e817aebecf48e6f97057ee03434c1373
Rhuwell Samano <rhuwell.samano@gmail.com>
2018-10-29 08:00:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
 
-function destructivelyPrepandKitten(name){
+function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
