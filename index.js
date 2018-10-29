cff66edd95a0477c18211d4fe45a48f34656a9e8
Rhuwell Samano <rhuwell.samano@gmail.com>
2018-10-29 08:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function prependKitten(name){
 }
 
 function removeFirstKitten(){
-  kittens.slice(1)
+  return kittens.slice(1)
 }
 
 function removeLastKitten(){
-  kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1)
 }
